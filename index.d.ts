919d5ac7321706496bc46f10269ef89f748380c4
Daniel Torres <dani@localhost>
2020-06-04 18:38:52 +0200

    first commit

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export { default as ButcherTableau, rk4ButcherTableau } from "./classes/butcher-tableaux.class";
+export { default as RungeKutta } from "./classes/runge-kutta.class";
---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var butcher_tableaux_class_1 = require("./classes/butcher-tableaux.class");
+Object.defineProperty(exports, "ButcherTableau", { enumerable: true, get: function () { return butcher_tableaux_class_1.default; } });
+Object.defineProperty(exports, "rk4ButcherTableau", { enumerable: true, get: function () { return butcher_tableaux_class_1.rk4ButcherTableau; } });
+var runge_kutta_class_1 = require("./classes/runge-kutta.class");
+Object.defineProperty(exports, "RungeKutta", { enumerable: true, get: function () { return runge_kutta_class_1.default; } });
+//# sourceMappingURL=index.js.map
